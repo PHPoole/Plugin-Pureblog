5f54772e2438a05b2199df17eb8ec69962175900
Arnaud Ligny <arnaud@ligny.org>
2014-02-17 18:18:57 +0100

    1rst commit

---
+++ Plugin.php
@@ -0,0 +1,29 @@
+<?php
+namespace PHPoole;
+use PHPoole\Utils;
+
+/**
+ * PHPoole plugin Pureblog
+ */
+Class Pureblog extends Plugin
+{
+    public function preInit($e)
+    {
+        $phpoole = $e->getTarget();
+        $phpoole->addMessage('Pureblog plugin available');
+    }
+
+    public function postInit($e)
+    {
+        $phpoole = $e->getTarget();
+        Utils\RecursiveCopy(
+            __DIR__ . '/assets',
+            $phpoole->getWebsitePath() . '/' . PHPoole::PHPOOLE_DIRNAME . '/' . PHPoole::ASSETS_DIRNAME
+        );
+        Utils\RecursiveCopy(
+            __DIR__ . '/layouts',
+            $phpoole->getWebsitePath() . '/' . PHPoole::PHPOOLE_DIRNAME . '/' . PHPoole::LAYOUTS_DIRNAME
+        );
+        $phpoole->addMessage('Pureblog layouts and assets copied');
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Plugin-Pureblog
-===============
+Bootsite plugin is a default [Twitter Bootstrap](http://getbootstrap.com) theme for [PHPoole](http://github.com/Narno/PHPoole/).
 
-Pure CSS blog layouts
+Installation
+------------
+
+    php composer.phar require phpoole/plugin-bootsite:@dev --prefer-dist
+
+
+Usage
+-----
+
+Just run a new website (```--init```) and see the magic!
